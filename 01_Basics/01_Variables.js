19678573dca409a98ac6e749bded88daa7f42713
Vipin Yadav <115709178+Yadavvipin09@users.noreply.github.com>
2024-09-26 09:10:01 +0000

    variables config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 23456
+let accountEmail= "vipin12@gmail.com"
+var accountPassword="343412"
+accountCity= "gurgaon"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
