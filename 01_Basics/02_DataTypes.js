78f9ad1c378eee4efd7aebd378023eeb76a29ae5
Vipin Yadav <115709178+Yadavvipin09@users.noreply.github.com>
2024-09-26 10:28:11 +0000

    Data Types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3+3)  we are using nodeJS not any browser
+
+console.log(3+3);
+
+console.log("vipin");
+
+let name ="vipn"
+let age = 22
+let isloggedIn = true
+let state;  //undefined
+
+
+// Numbers => 2 to the power 53
+// bigint => used in large numbers like trading,marketing etc
+// string => ""
+// boolean => yes/no
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object =>
+
+
+console.log(typeof age);
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined0
+
+console.log(state);
+
+
+
