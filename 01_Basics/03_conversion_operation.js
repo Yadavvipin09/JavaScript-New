920a5bcdad0bf09337845b2bf78e542ee35c7a14
Vipin Yadav <115709178+Yadavvipin09@users.noreply.github.com>
2024-09-26 11:22:41 +0000

    Conversion Operation

---
+++ 01_Basics/03_conversion_operation.js
@@ -0,0 +1,24 @@
+let score = 97
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+
+// "31" => 31
+// " 33abc" => NaN
+// true=1 / false=0
+
+let isloggedIn = 0
+let booleanisloggedIn = Boolean(isloggedIn)
+console.log(booleanisloggedIn);
+
+
+let SN = 33
+let stringNumber = String(SN)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
